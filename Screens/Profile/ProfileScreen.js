4bc3f56379a7def4ab5c1327302c1a2aa8148477
SonMinSeock <zonins3@gmail.com>
2022-03-04 19:40:17 +0900

    프로필 컴포넌트firebase 연결, 미완성(2022-03-04)

--- Screens/Profile/ProfileScreen.js
+++ Screens/Profile/ProfileScreen.js
@@ -1,18 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
 // 아이콘(원격주소)
 import { Fontisto } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
+//firebase
+import { db } from '../../Database/DatabaseConfig/firebase';
+import { doc, getDoc } from 'firebase/firestore';
+// 회원정보 데이터
+import { UserInfo } from'../../Database/Data/User/userInfo';
 
 export default function ProfileScreen({navigation})  {
+    useEffect(() => {
+        Read();
+    }, []);
+
+    // firebase로 불러온 정보를 선언(가리키고자)하고자 한다.
+    let readUserDoc;
+    // 사용자 티켓들 
+    let userTickets;
+
+    // 데아터 문서 읽어오기
+    const Read = () => {
+        const myDoc = doc(db, "CollectionNameCarpoolTicket", "CarpoolTicketDocument");
+        
+        getDoc(myDoc)
+        .then((snapshot) => {
+            if (snapshot.exists) {
+                
+                readUserDoc = snapshot.data();
+                console.log("firebase로부터 불러온 회원 정보들 : ", readUserDoc.CarpoolTicket);
+                console.log("회원정보 : ", UserInfo.UserInfo[0]);
+                readUserDoc = readUserDoc.CarpoolTicket;
+                ShowTickets();
+            }
+        })
+        .catch((error) => {
+            alert(error.messeage);
+        });
+    }
+
+    const ShowTickets = () => {
+        return(
+            [
+            <View style={styles.ticket}>
+                <View>
+                    {() => Read()}
+                </View>
+                <View style={{flexDirection:'row', padding: 10, borderBottomWidth: 2, borderColor: "#F0F0F0"}}>
+                    <Text style={{marginLeft: 20, fontSize: 16,}}>Order No238562312</Text>
+                    <Text style={{marginLeft: 45, fontSize: 13, color: "#909090"}}>2022/03/01</Text>
+                </View>
+
+                <View style={{flexDirection: 'row', marginTop: 5}}>
+                    <Text style={{marginLeft: 30, fontSize: 16,}}>Quantity: 03</Text>
+                    <Text style={{marginLeft: 36, fontSize: 16,}}>Total Amount: $150</Text>
+                </View>
+
+                <View style={{flexDirection: 'row', marginTop: 35}}>
+                    <Text style={{marginLeft: 30, fontSize: 16,}}>Detail</Text>
+                    <Text style={{marginLeft: 145, fontSize: 16,}}>Processing</Text>
+                </View>
+            </View>
+            ]
+        );
+    }
+
 
     return (
         <View style={styles.container}>
-            
             <View style={styles.header}>
                 <View style={styles.profile_info}>
                     <Image style={styles.profile_img} source={require('../../profile_img1.jpeg')}/>
-                    <Text style={styles.user_name_text}>손민석</Text>
+                    <Text style={styles.user_name_text}>{UserInfo.UserInfo[0].nickname}</Text>
                     
                 </View>
 
@@ -54,24 +113,7 @@ export default function ProfileScreen({navigation})  {
             </View>
             <View style={styles.center}>
                 <ScrollView>
-                    
-                    <View style={styles.ticket}>
-                        <View style={{flexDirection:'row', padding: 10, borderBottomWidth: 2, borderColor: "#F0F0F0"}}>
-                            <Text style={{marginLeft: 20, fontSize: 16,}}>Order No238562312</Text>
-                            <Text style={{marginLeft: 45, fontSize: 13, color: "#909090"}}>2022/03/01</Text>
-                        </View>
-
-                        <View style={{flexDirection: 'row', marginTop: 5}}>
-                            <Text style={{marginLeft: 30, fontSize: 16,}}>Quantity: 03</Text>
-                            <Text style={{marginLeft: 36, fontSize: 16,}}>Total Amount: $150</Text>
-                        </View>
-
-                        <View style={{flexDirection: 'row', marginTop: 35}}>
-                            <Text style={{marginLeft: 30, fontSize: 16,}}>Detail</Text>
-                            <Text style={{marginLeft: 145, fontSize: 16,}}>Processing</Text>
-                        </View>
-                    </View>
-
+                    {ShowTickets()}
                     <View style={styles.ticket}>
                         <View style={{flexDirection:'row', padding: 10, borderBottomWidth: 2, borderColor: "#F0F0F0"}}>
                             <Text style={{marginLeft: 20, fontSize: 16,}}>Order No238562312</Text>
